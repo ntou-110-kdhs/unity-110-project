db74986307674480e314fb307ca1eb0daf6a0497
konohaDX <50859899+konohaDX@users.noreply.github.com>
2020-05-09 01:12:03 +0800

    PlayerController.cs shadowDetect
    能偵測出你在哪個物件的影子上

--- _110_animation_project/Assets/tmp_script/director/TestPlayerController.cs
+++ _110_animation_project/Assets/tmp_script/director/TestPlayerController.cs
@@ -12,25 +12,28 @@ using UnityEngine;
 
 public class TestPlayerController : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> lights;
-    [SerializeField] private bool isAutoFindLights = false;
 
+    //你人是否在影子內
+    private bool isInShadow = false;                
+    //光源的陣列
+    private List<GameObject> lights = new List<GameObject>();
+    //一個光源對一個物件所製造出的影子  陣列
+    private Dictionary<string, GameObject> lightsWithShadows = new Dictionary<string, GameObject>();
+    
     // Start is called before the first frame update
     void Start()
     {
-        if (isAutoFindLights)
-        {
-            findAllLightsInScene();
-        }
+        //載入所有光源
+        findAllLightsInScene();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (shadowDetect())
-        {
-            Debug.Log("isInShadow");
-        }
+        // 偵測影子
+        shadowDetect();
+        // 印出你踩在哪個影子上
+        printWhatShadowsIn();
     }
 
     /// <summary>
@@ -115,24 +118,26 @@ public class TestPlayerController : MonoBehaviour
          *  parameter: 
          *  None
          *  member var need:
-         *  [SerializeField] private List<GameObject> lights      存放所有光源物件的陣列;   
+         *  private bool isInShadow
+         *  private List<GameObject> lights         
+         *  private Dictionary<string, GameObject> lightsWithShadows
          *  
          *  Update();     
-         *  回傳  是否在影子內
+         *  回傳  void        
          *  偵測影子用
          */
-    private bool shadowDetect()
+    private void shadowDetect()
     {
+        isInShadow = false;
+        Vector3 playerPos = transform.position;
+        playerPos.y += 0.5f;
         for (int i = 0; i < lights.Count; i++)
         {
-            float distance = 0.0f;
-            Vector3 playerPos = transform.position;
-            playerPos.y += 0.5f;
+            float distance = 0.0f;            
             Light lightCompnent = lights[i].GetComponent<Light>();
 
             if (lightCompnent.type.ToString() == "Directional")
             {
-
                 //Debug.Log("Directional Light");
                 // 太陽位置設定
                 // 假設太陽距離(很遠)
@@ -142,86 +147,92 @@ public class TestPlayerController : MonoBehaviour
                 // 人和太陽實際距離
                 distance = Vector3.Distance(sunPos, playerPos);
 
-
                 // ray 設定
                 // ray 起點 => 太陽位置， 方向 => 玩家位置 - 太陽位置
                 Ray ray = new Ray(sunPos, (playerPos - sunPos));
                 RaycastHit hit;
                 Debug.DrawRay(ray.origin, ray.direction, Color.red);
 
-
-                if (Physics.Raycast(ray, out hit, distance))
-                {
-                    // 光線擋到物體不可以是玩家
-                    if (hit.transform != transform)
+                // 光線擋到物體不可以是玩家
+                if (Physics.Raycast(ray, out hit, distance) && hit.transform != transform)
+                {                  
+                    //Debug.Log("Directional light make you in shadow");
+                    if(lightsWithShadows[lights[i].name] != hit.transform.gameObject)
                     {
-                        //Debug.Log("Directional light make you in shadow");
-
-                        return true;
+                        lightsWithShadows[lights[i].name] = hit.transform.gameObject;
                     }
+                    isInShadow = true;                    
+                }
+                else
+                {
+                    lightsWithShadows[lights[i].name] = null;
                 }
             }
             else
             {
                 distance = Vector3.Distance(lights[i].transform.position, playerPos);
-                Ray ray = new Ray(lights[i].transform.position, (playerPos - lights[i].transform.position));
-                RaycastHit hit;
-                Debug.DrawRay(ray.origin, ray.direction, Color.red);
-                if (distance <= lightCompnent.range)
+                // Point light 的判定
+                if (lightCompnent.type.ToString() == "Point")
                 {
-                    // Point light 的判定
-                    if (lightCompnent.type.ToString() == "Point")
-                    {
-                        //Debug.Log("SpotLight");
-                        // 判定光線有沒被物體檔到
-                        if (Physics.Raycast(ray, out hit, distance))
+                    Ray ray = new Ray(lights[i].transform.position, (playerPos - lights[i].transform.position));
+                    RaycastHit hit;
+                    Debug.DrawRay(ray.origin, ray.direction, Color.red);
+                    //Debug.Log("SpotLight");
+                    // 判定有沒有在光線範圍內
+                    // 判定光線有沒被物體檔到
+                    // 光線擋到物體不可以是玩家
+                    if (distance <= lightCompnent.range && Physics.Raycast(ray, out hit, distance) && hit.transform != transform)
+                    {                        
+                        if (lightsWithShadows[lights[i].name] != hit.transform.gameObject)
                         {
-                            // 光線擋到物體不可以是玩家
-                            if (hit.transform != transform)
-                            {
-                                //Debug.Log("Point Light make you in shadow");
-                                Debug.Log(hit.transform.name);
-                                return true;
-                            }
+                            lightsWithShadows[lights[i].name] = hit.transform.gameObject;
                         }
+                        isInShadow = true;
                     }
-                    // Spot light 的判定
                     else
                     {
-                        Vector3 dir = playerPos - lights[i].transform.position;
-                        float angle = Vector3.Angle(dir, lights[i].transform.forward);
-
-                        // 判定有沒有在光線範圍內
-                        if (angle <= lightCompnent.spotAngle / 2)
-                        {
-                            //判定光線有沒有被物體檔到
-                            if (Physics.Raycast(ray, out hit, distance))
-                            {
-                                // 光線擋到物體不可以是玩家
-                                if (hit.transform != transform)
-                                {
-                                    //Debug.Log("Spot light make you in shadow");
-                                    return true;
-                                }
-                            }
-                        }
+                        lightsWithShadows[lights[i].name] = null;
                     }
+                }
+                // Spot light 的判定 
+                else
+                {
+                    Ray ray = new Ray(lights[i].transform.position, (playerPos - lights[i].transform.position));
+                    RaycastHit hit;
+                    Debug.DrawRay(ray.origin, ray.direction, Color.red);
+                    Vector3 dir = playerPos - lights[i].transform.position;
+                    float angle = Vector3.Angle(dir, lights[i].transform.forward);                    
+                    // 判定有沒有在光線範圍內
+                    // 判定光線有沒有被物體檔到
+                    // 光線擋到物體不可以是玩家
+                    if(distance <= lightCompnent.range && angle <= lightCompnent.spotAngle / 2 && Physics.Raycast(ray, out hit, distance) && hit.transform != transform)
+                    {
+                        //Debug.Log("Spot light make you in shadow");
+                        if (lightsWithShadows[lights[i].name] != hit.transform.gameObject)
+                        {
+                            lightsWithShadows[lights[i].name] = hit.transform.gameObject;
+                        }
+                        isInShadow = true;
+                    }
+                    else
+                    {
+                        lightsWithShadows[lights[i].name] = null;
+                    }                    
                 }
             }
             //Debug.Log(lights[i].name);
-        }
-        return false;
+        }        
     }
-
     /*  findAllLightsInScene()
      *  parameter: 
      *  None
      *  member var need:
-     *  [SerializeField] private bool isAutoFindLights       是否自動找場景內所有光源           
+     *  None
      *  
      *  Start();
      *  回傳  void
      *  自動把所有場景內的光源抓進陣列內
+     *  
      */
     private void findAllLightsInScene()
     {
@@ -229,6 +240,28 @@ public class TestPlayerController : MonoBehaviour
         foreach (Light light in lightArr)
         {
             lights.Add(light.gameObject);
+            lightsWithShadows.Add(light.transform.name, null);
+        }
+    }
+    /*  printWhatShadowsIn()
+     *  parameter: 
+     *  None
+     *  member var need:
+     *  private bool lightsWithShadows                
+     *  
+     *  Start();
+     *  回傳  void
+     *  只是印出你在哪個物件的影子內
+     *  
+     */
+    private void printWhatShadowsIn()
+    {
+        foreach (KeyValuePair<string, GameObject> i in lightsWithShadows)
+        {
+            if (i.Value != null)
+            {
+                Debug.Log("you are in " + i.Value.transform.name + " 's shadow");
+            }
         }
     }
 
