a171d7c359f761758e4ce0e1ebcc1ce949bbd7ed
konohaDX <50859899+konohaDX@users.noreply.github.com>
2020-11-27 16:47:35 +0800

    push 進度

---
+++ _110_animation_project/Assets/tmp_script/director/MiniMapController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapController : MonoBehaviour
+{
+    private Transform player;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.transform.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
